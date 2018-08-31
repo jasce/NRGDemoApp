c23ae87d363991b2cb8a2ffa9c0066e0bf83ddb0
Jasmeet Singh <jascee.gtbit@gmail.com>
2018-08-31 18:56:58 +0530

    chore: enabled cors

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require("./schema/schema");
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross origin requests
+
+app.use(cors());
 
 mongoose.connect('mongodb://jascee:test123@ds019996.mlab.com:19996/gql-test')
 
